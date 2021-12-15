89c90f32c9e05d72361769b9b1f94b1eef8faffd
Patricia-Bekyere <91859038+Patricia-Bekyere@users.noreply.github.com>
2021-12-15 06:51:59 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+import { animals } from './animals';
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const title = '';
+const background = (
+  <div>
+<img src ='/images/ocean.jpg' className = 'background' alt = 'ocean' />
+  </div>
+)
+
+const images = [];
+
+const showBackground = true;
+
+for (const animal in animals) {
+  images.push(
+  <img
+  key ={animal}
+  className = 'animal'
+  alt = {animal}
+  src = {animals[animal].image}
+  ariaLabel = {animal}
+  role = 'button'  
+  onClick = {displayFact}
+  />
+  )
+}
+
+function displayFact(e) {
+const selectedAnimal = e.target.alt;
+const animalInfo = animals[selectedAnimal];
+const optionIndex = Math.floor(Math.random()* animalInfo.facts.length);
+
+const funFact = animalInfo.facts[optionIndex]; document.getElementById('fact').innerHTML = funFact;
+}
+
+const animalFacts = (
+  <div>
+    <h1>{title || 'Click an animal for a fun fact'}</h1>
+    {showBackground && background}
+    <p id='fact'></p>
+    <div className = 'animals'>
+    {images}
+    </div>
+  </div>
+)
+ReactDOM.render(animalFacts, document.getElementById('root'));
